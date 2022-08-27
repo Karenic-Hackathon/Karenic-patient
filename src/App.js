cec8da3c3e3f077645f84dfcd8904c0f00812cb4
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-08-27 21:32:59 +0200

    removed route to deleted component findPatient

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from './components/Header';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './pages/Home';
 import ReportIncident from './pages/ReportIncident';
-import FindPatient from './pages/findPatient';
 import PatientRecords from './pages/PatientRecords';
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
             <Routes>
                   <Route path='/' element={<Home/>}/>
                   <Route path='/reportIncident' element={<ReportIncident/>}/>
-                  <Route path='/findPatient' element={<FindPatient/> }/>
                   <Route path='/patients/records' element={<PatientRecords/>}/>
             </Routes>
           </div>
