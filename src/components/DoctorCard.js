56e9489da577c41650ea4f4f0e3cfefa26baa0ce
Nonelela4980 <nonelelacele@gmail.com>
2022-08-28 18:13:11 +0200

    update

--- src/components/DoctorCard.js
+++ src/components/DoctorCard.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import '../styles/doctorCard.css'
 import doctor from '../assets/images/doctor.png'
+import {useNavigate} from 'react-router-dom'
 export default function DoctorCard({fullname,speciality,placeOfWork}) {
+      const navigate = useNavigate()
   return (
-    <div className='doctor-card'>
+    <div className='doctor-card' onClick={()=>navigate('/doctors/1234')}>
         <div className='image-container-doctor'>
             <img src={doctor}/>
         </div>
